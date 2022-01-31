849021bd14bb23b356ca60a1a04a7b96b936ce60
Llaaamaguy <67341568+Llaaamaguy@users.noreply.github.com>
2022-01-31 10:36:57 -0500

    Stuff

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import os
 from os.path import exists
 import random
 import pandas as pd
 import io
+from IPython.display import HTML
+import time
+import threading
 
 app = Flask(__name__)
 
@@ -19,6 +22,12 @@ buf = io.StringIO()
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def del_file(htmlfile, datafile):
+    time.sleep(60)
+    os.remove(htmlfile)
+    os.remove(datafile)
 
 
 @app.route("/")
@@ -45,6 +54,7 @@ def handle_data():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            datafile = filepath
 
             while exists(filepath):
                 fnameext = filename.split(".")
@@ -52,21 +62,33 @@ def handle_data():
                 ext = fnameext[1]
                 filename = fname + str(random.randint(1, 10)) + "." + str(ext)
                 filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+                datafile = filepath
 
             file.save(filepath)
-
             data = pd.read_csv(filepath)
 
-            columns = data.columns
+            htmldata = data.to_html(classes="table table-striped")
+            fname = "templates/viewData" + str(random.randint(1,9999999)) + ".html"
+            while exists(fname):
+                fname = fname.split(".")
+                file = fname[0]
+                ext = fname[1]
+                fname = file + str(random.randint(1,10)) + "." + str(ext)
 
+            with open(fname, "w") as f:
+                f.write(htmldata)
 
+            t = threading.Thread(target=del_file, args=(fname, datafile)).start()
 
-            return ""
+            fname = fname.split("/")
+            fname = fname[1]
 
-        return "file uploaded"
+            return render_template(fname)
+
+        return jsonify({"message": "file opened :thunbs2up:!"})
 
     if request.method == "GET":
-        return "This was made by a get method?"
+        return jsonify({"message": "This was made by a get method?"})
 
 
 if __name__ == "__main__":
